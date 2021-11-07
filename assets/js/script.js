2ac6fde274fa06f30c45ad66892a1994884c92c8
Andrew Leonard <AndrewCLeonard@fastmail.com>
2021-11-06 17:27:20 -0700

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,9 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
 
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
+
 var taskFormHandler = function (event) {
 	event.preventDefault();
 	var taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -24,14 +27,25 @@ var taskFormHandler = function (event) {
 	}
 
 	formEl.reset();
-	// package up data as an object
-	var taskDataObj = {
-		name: taskNameInput,
-		type: taskTypeInput,
-	};
 
-	// send it as an argument to createTaskEl
-	createTaskEl(taskDataObj);
+	var isEdit = formEl.hasAttribute("data-task-id");
+
+	// has data attribute, so get task id and call function ot complete edit process
+	if (isEdit) {
+		var taskId = formEl.getAttribute("data-task-id");
+		completeEditTask(taskNameInput, taskTypeInput, taskId);
+	}
+
+	// no data attribute, so create object as normal and pass to createTaskEl function
+	else {
+		var taskDataObj = {
+			name: taskNameInput,
+			type: taskTypeInput,
+		};
+
+		// send it as an argument to createTaskEl
+		createTaskEl(taskDataObj);
+	}
 };
 // creates: TaskEl, taskId, taskInfoEl, taskActionsEl, & increments counter
 var createTaskEl = function (taskDataObj) {
@@ -101,6 +115,25 @@ var createTaskActions = function (taskId) {
 
 	return actionContainerEl;
 };
+
+var completeEditTask = function (taskName, taskType, taskId) {
+	// find the matching task list item
+	var taskSelected = document.querySelector(
+		".task-item[data-task-id='" + taskId + "']"
+	);
+
+	// set new values
+	taskSelected.querySelector("h3.task-name").textContent = taskName;
+	taskSelected.querySelector("span.task-type").textContent = taskType;
+
+	alert("Task Updated!");
+
+	// reset form by removing the task id and changing the button text back to normal
+	formEl.removeAttribute("data-task-id");
+	// update formEl button to go back to saying "Add Task" instead of "Edit Task"
+	formEl.querySelector("#save-task").textContent = "Add Task";
+};
+
 // task button functionality
 var taskButtonHandler = function (event) {
 	// get target element from event
@@ -116,6 +149,31 @@ var taskButtonHandler = function (event) {
 		deleteTask(taskId);
 	}
 };
+
+// change task status
+var taskStatusChangeHandler = function (event) {
+	// get the task item's id
+	var taskId = event.target.getAttribute("data-task-id");
+
+	// get the currently selected option's value and convert to lowercase
+	var statusValue = event.target.value.toLowerCase();
+
+	// find the parent task item element based on the id
+	var taskSelected = document.querySelector(
+		".task-item[data-task-id='" + taskId + "']"
+	);
+
+	if (statusValue === "to do") {
+		tasksToDoEl.appendChild(taskSelected);
+	}
+	else if (statusValue === "in progress") {
+		tasksInProgressEl.appendChild(taskSelected);
+	}
+	else if (statusValue === "completed") {
+		tasksCompletedEl.appendChild(taskSelected);
+	}
+};
+
 // task edit functionality
 var editTask = function (taskId) {
 	// get task list item element
@@ -129,7 +187,8 @@ var editTask = function (taskId) {
 	var taskType = taskSelected.querySelector("span.task-type").textContent;
 	document.querySelector("input[name='task-name']").value = taskName;
 	document.querySelector("select[name='task-type']").value = taskType;
-	document.querySelector("#save-task").textContent = "Save Task";
+
+	// set data attribute to the form with a value of the task's id so it knows which one is being edited
 	formEl.setAttribute("data-task-id", taskId);
 };
 
@@ -140,8 +199,10 @@ var deleteTask = function (taskId) {
 	);
 	taskSelected.remove();
 };
+
 // event listener: reate a new task
 formEl.addEventListener("submit", taskFormHandler);
 
 // event listener: for edit and delete buttons
 pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
